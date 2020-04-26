f80802720569e4b587e9e005ec43d5f4873e2a73
crycetruly <crycetruly@gmail.com>
2020-04-26 20:37:42 +0200

    ch(naming): Fix Category naming

--- expenses/models.py
+++ expenses/models.py
@@ -24,5 +24,8 @@ class Expense(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
