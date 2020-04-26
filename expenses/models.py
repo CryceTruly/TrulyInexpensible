f0707a9a669bbc0dae7b885641f0ba916e23a671
crycetruly <crycetruly@gmail.com>
2020-04-26 07:08:34 +0200

    ch(data): Add ajax validations to forms

--- expenses/models.py
+++ expenses/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 import datetime
+from django.utils.timezone import now
 
 
 class Expense(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(default=now)
     amount = models.FloatField(blank=False, null=False)
     currency = models.CharField(max_length=20, default="USD")
     category = models.CharField(max_length=255)
